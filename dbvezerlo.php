a02ec3ab7692eb2fc478db22f7de91ee41c72547
Bakos Bence <bakosbence1@icloud.com>
2025-03-10 13:51:11 +0100

    igen

--- dbvezerlo.php
+++ dbvezerlo.php
@@ -1,39 +1,44 @@
 <?php
-class DBVezerlo {
-	private $conn = null;
-	private $host = "localhost";
-	private $user = "root";
-	private $password = "";
-	private $database = "oscar";
 
-	function __construct() {
-		$conn = $this->connectDB();
-		if(!empty($conn)) {
-			$this->conn = $conn;			
-		}
-	}
+class DBController
+{
+    private $conn = null;
 
-	function connectDB() {
-		$conn = mysqli_connect($this->host,$this->user,
-		$this->password,$this->database);
-		return $conn;
-	}
+    private $host = "Localhost";
 
-	function executeSelectQuery($query) {
-		$result = mysqli_query($this->conn,$query);
-		while($row=mysqli_fetch_assoc($result)) {
-			$resultset[] = $row;
-		}
-		if(!empty($resultset))
-			return $resultset;
-	}
+    private $user = "root";
 
-	function closeDB() {
-        if (!empty($this->conn)) {
-            mysqli_close($this->conn);
-            $this->conn = null;
-        }
+    private $password = "";
+    
+    private $dataase = "oscar";
+
+    function __construct()
+    {
+       $conn = $this->connectDB();
+       if(!empty($conn))
+       {
+            $this->conn = $conn;
+       }
     }
 
+    function connectDB()
+    {
+       $conn = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+       return $conn;
+    }
+    function executeSelectQuery($query,$params = [])
+    {
+        $result = mysqli_query($this->conn,$query);
+        while($row=mysqli_fetch_assoc($result))
+        {
+            $resultset[] = $row;
+        }
+        if(!empty($resultset))
+        return $resultset;
+    }
+    function closeDB()
+    {
+        mysqli_close($this->conn);
+        $this->conn=null;
+    }
 }
-?>
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-echo "Hello World!";
+require_once("restVezerlo.php");
 ?>
